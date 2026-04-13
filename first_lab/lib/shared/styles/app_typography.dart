2ad3b70959953f437fd33cc18359549184aeda02
Danyil Serafin <serafin.danyil14@gmail.com>
2026-04-13 17:33:55 +0300

    fix: adjust font sizes in IndicatorCard and AppTypography for consistency

--- first_lab/lib/shared/styles/app_typography.dart
+++ first_lab/lib/shared/styles/app_typography.dart
@@ -1,12 +1,12 @@
 abstract final class AppTypography {
-  static const double baseRem = 28; //FIXME: Перемножити на 16
+  static const double baseRem = 26;
 
-  static const double header1 = 28;
-  static const double header2 = 24.08;
-  static const double header3 = 17.92;
+  static const double header1 = 26;
+  static const double header2 = 22.36;
+  static const double header3 = 16.64;
 
-  static const double text1 = 15.96;
-  static const double text2 = 14;
-  static const double text3 = 12.04;
-  static const double text4 = 10.08;
+  static const double text1 = 14.82;
+  static const double text2 = 13;
+  static const double text3 = 11.18;
+  static const double text4 = 9.36;
 }
